44cf1629f164c73316f2221f7b9d9e06cecdbc28
Chanwoo1124 <lgw22774@gmail.com>
2025-08-26 23:00:21 +0900

    ServletContext.getAttribute

--- src/main/java/com/newlecture/web/Calc2.java
+++ src/main/java/com/newlecture/web/Calc2.java
@@ -3,6 +3,7 @@ package com.newlecture.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,30 +13,40 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/calc2") 
 public class Calc2 extends HttpServlet {
 		protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		ServletContext application = request.getServletContext();
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		
 		PrintWriter out = response.getWriter();
 		
-		String x_ = request.getParameter("x");
-		String y_ = request.getParameter("y");
+		String v_ = request.getParameter("v");
 		String op = request.getParameter("operator");
 		
 		
-		int x = 0;
-		int y = 0;
-		
-		if (!x_.equals("0")) x = Integer.parseInt(x_);
-		if (!y_.equals("0")) y = Integer.parseInt(y_);
-		
-		int result = 0;
-		if(op.equals("덧셈")) {
-			result = x + y;
-		}else {
-			result = x - y;
+		int v = 0;
+		if (!v_.equals("")) v = Integer.parseInt(v_);
+		//계산
+		if(op.equals("=")) {
+			
+			int x = (Integer) application.getAttribute("value");
+			int y = v;
+			
+			String operator = (String) application.getAttribute("op");
+			int result = 0;
+			if(operator.equals("+")) {
+				result = x + y;
+			}else {
+				result = x - y;
+			}
+			response.getWriter().printf("result is %d\n", result);
+			
 		}
-		out.println(result);
+		//값을 저장 
+		else {
+			application.setAttribute("value", v);
+			application.setAttribute("op", op);
+		}
+		
 		
 	}
 }	
